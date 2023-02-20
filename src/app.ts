3153c7c36efec0c86ed8a3041c1291068e1ee9d0
Patrick Mazara <patrickmazara@hotmail.com>
2023-02-20 17:23:05 -0500

    add winning combos

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('testing')
+const winningCombo: number[][] = [
+      // Horizonal Combos
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Vertical Combos
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Diagonal Combos
+      [0, 4, 8],
+      [2, 4, 6],
+  
+];
+
+
